1c7beaa1f0235b7770f04c0e7670899505ccbbd6
Jherson Villa Arcibia <jherson2408va@gmail.com>
2024-09-02 19:21:16 -0500

    Actualización del backend: [Edit del comentario en mi controller FraseController]

--- screenmatch-frases-backend/src/main/java/com/desafioalura/screenmatch_frases/controller/FraseController.java
+++ screenmatch-frases-backend/src/main/java/com/desafioalura/screenmatch_frases/controller/FraseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.desafioalura.screenmatch_frases.service.FraseService;
 
-//Clase FraseController
+//Controlador FraseController
 @RestController
 public class FraseController {
 
